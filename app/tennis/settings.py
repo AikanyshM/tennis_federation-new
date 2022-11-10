2dce7513ac58ed795d42a853bf361ec0f246be11
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-10 13:38:10 +0600

    Last updates

--- app/tennis/settings.py
+++ app/tennis/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=0)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-
+ALLOWED_HOSTS = ['127.0.0.1',] if not os.environ.get('ALLOWED_HOSTS') else os.environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
