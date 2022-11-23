a156f328903a94deb6f4583d0d9166e898c7fe01
Kamila Sulaimanova <sulaimanovakamila@gmail.com>
2022-11-23 20:20:04 +0600

    Rebase

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import RegisterView, ChangePasswordView, LogoutView, LogoutAllView, UserProfile, AdminUserCreateAPIView
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from .views import RegisterView, ChangePasswordView, LogoutView, LogoutAllView, UserProfile, AdminUserCreateAPIView, MyObtainTokenPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('create-admin/', AdminUserCreateAPIView.as_view()),
--- app/tennis/urls.py
+++ app/tennis/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -21,16 +23,17 @@ schema_view = get_schema_view(
 
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('first_app.urls')),
     path('auth/', include('account.urls')),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
-    path('api-json', schema_view.without_ui(cache_timeout=0), name='api-json')
+    path('api-json', schema_view.without_ui(cache_timeout=0), name='api-json'),
+    path('rosetta/', include('rosetta.urls')),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
