349226169b986a613647cf7ec2ce3f6c9d8788ec
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2022-11-29 18:34:31 +0600

    Changed text field to Char Field in address

--- app/first_app/models.py
+++ app/first_app/models.py
@@ -30,7 +30,7 @@ class Club(TranslatableModel):
     translations = TranslatedFields(
         name = models.CharField(max_length=100, verbose_name = _("Название клуба")),
         description = models.CharField(max_length=255, verbose_name = _("Описание клуба")),
-        address =   models.TextField(verbose_name = _("Адрес клуба")),
+        address =   models.CharField(max_length=100, verbose_name = _("Адрес клуба")),
         contacts = models.CharField(max_length=100, verbose_name = _("Контакты")),
         working_hours = models.CharField(max_length=100, verbose_name = _("Часы работы"))
         )
