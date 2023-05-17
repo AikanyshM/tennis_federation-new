f7336698e9b4d5ada27b78413fbd694ef8e4ea55
Aikanysh Matyeva <aikanyshmatyeva@MacBook-Air-Elmira.local>
2023-05-17 16:36:38 +0600

    updated total first_app

--- app/first_app/permissions.py
+++ app/first_app/permissions.py
@@ -5,9 +5,9 @@ class IsStaffOrAny(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        # elif request.user.is_superuser or request.user.is_staff:
-        #     return True
-        elif request.user.is_authenticated:
+        elif request.user.is_superuser or request.user.is_staff:
             return True
+        #elif request.user.is_authenticated:
+            #return True
 
 
